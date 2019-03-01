3c433cda5a0219347ae318edee61554bac63c8b3
root <root@phpdaw2.fjeclot.net>
2019-03-01 19:11:22 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: https://github.com/damaci/pluginwp2_davman
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: David Manzano
+Author URI: https://github.com/damaci
+License: GNU GPLv3 
+*/ 
+?>
