f86e5aab0e26ea9243024e08f8e8666d25376eef
root <root@phpdaw2.fjeclot.net>
2019-03-08 17:16:10 +0100

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -1,25 +1,29 @@
 <?php
 /*
 Plugin Name: Peu de pàgina per defecte per a noves entrades 
-Plugin URI: https://github.com/damaci/pluginwp2_davman
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: David Manzano
-Author URI: https://github.com/damaci
+Author: David Manzano Cisneros
+Author URI: http://www.github.com/damaci
 License: GNU GPLv3 
-*/ 
-function admin_menu_peu_pagina(){ 
+*/
+function admin_menu_peu_pagina(){
 	$titolPagina="Peu de pàgina per defecte";
 	$titolEntrada="Peu de pàgina per defecte";
 	$role="administrator";
 	$id="peu_pagina_per_defecte";
 	$nom_funcio="nouPeuPagDef";
-	add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
+	add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio); 
 }
-
 function nouPeuPagDef(){
 	include_once('formPeuPag.php');
 }
-
-add_action('admin_menu','admin_menu_peu_pagina','nouPeuPagDef');
+function afegPeuPagDef(){
+    echo "<div style='color: red;
+    font-size: 10px;
+    margin: 10px;'>".get_option('footer_text')."</div>";
+}
+add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
 ?>
